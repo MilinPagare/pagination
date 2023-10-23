3aa3df824a346c1b08d5dd04a1d8b3a82c8de6d1
MilinPagare <pagare.milin1996@gmail.com>
2023-10-23 18:31:38 +0530

    basic pagination added

--- src/App.js
+++ src/App.js
@@ -3,56 +3,9 @@ import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-  const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(false);
-
-  useEffect(() => {
-    let intervalId;
-    if (running) {
-      intervalId = setInterval(() => {
-        setTime((time) => time + 1);
-      }, 10);
-    }
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [running]);
-
-  const miliSec = time % 100;
-  const seconds = Math.floor((time % 6000) / 100);
-  const minutes = Math.floor((time % 360000) / 6000);
-  const hours = Math.floor(time / 360000);
-
   return (
     <div className="App">
-      <div>
-        <div>
-          <h1>Timer</h1>
-        </div>
-        <div>
-          {hours < 10 ? "0" + hours : hours} :{" "}
-          {minutes < 10 ? "0" + minutes : minutes} :{" "}
-          {seconds < 10 ? "0" + seconds : seconds} :{" "}
-          {miliSec < 10 ? "0" + miliSec : miliSec}
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              setRunning(true);
-            }}
-          >
-            Start
-          </button>
-          <button
-            onClick={() => {
-              setRunning(false);
-            }}
-          >
-            Pause
-          </button>
-        </div>
-      </div>
-      {/* <Home/> */}
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,40 @@
 import React, { useEffect, useState } from "react";
-import {Box,Grid,ImageList,ImageListItem} from "@mui/material";
 
-const Home = () => {
-    const [images, setImages] = useState([]);
-    const [imgToDisplay, setImgToDisplay] = useState([]);
-    const [load, setLoad] = useState(20);
-    const [toAdd, setToAdd] = useState(0);
-
-    const getImages = async() => {
-        const result = await fetch("https://jsonplaceholder.typicode.com/photos");
-        const temp = await result.json();
-        setImages(temp);
-        setImgToDisplay(temp.slice(0,load));
-        setToAdd(load);
-    }
-
-    const handleScroll = () => {
-        const userScrollHeight = window.innerHeight+window.scrollY;
-        const windowBottomHeight = document.documentElement.offsetHeight;
-        if(userScrollHeight>=windowBottomHeight){
-            // getImages();
-            setImgToDisplay(images.slice(toAdd, toAdd+load));
-            setToAdd(toAdd+load);
-        }
-    }
-
-    useEffect(()=>{
-        getImages();
-        window.addEventListener('scroll', handleScroll);
-    },[]);
-// https://jsonplaceholder.typicode.com/photos
-// https://picsum.photos/v2/list
-    return (
-        <Box>
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {imgToDisplay.map((item) => (
-                    <Grid item xs={2} sm={4} md={4} key={item.id}>
-                        <h1>hello</h1>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
+const userData = new Array(25);
+let count = 1;
+for (let i = 0; i < userData.length; i++) {
+  userData[i] = count;
+  count++;
 }
+const Home = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage, setItemPerPage] = useState(10);
+
+  const indexOfLastItem = itemPerPage * currentPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+
+  const arrToPrint = userData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const next = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  const previous = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  return (
+    <div>
+      {arrToPrint.map((item, index) => (
+        <div key={index}>
+          <h3>{item}</h3>
+        </div>
+      ))}
+      <div>
+        <button onClick={previous}>Prev</button>
+        <button onClick={next}>Next</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
