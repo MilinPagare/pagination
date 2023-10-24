3c9a632209a8996b9b1e8a55f40949a6360ef310
MilinPagare <pagare.milin1996@gmail.com>
2023-10-24 12:51:24 +0530

    paginaiton completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const userData = new Array(25);
+const userData = new Array(105);
 let count = 1;
 for (let i = 0; i < userData.length; i++) {
   userData[i] = count;
@@ -8,32 +8,93 @@ for (let i = 0; i < userData.length; i++) {
 }
 const Home = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemPerPage, setItemPerPage] = useState(10);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [min, setMin] = useState(0);
+  const [max, setMax] = useState(5);
+  const [pageLimit, setPageLimit] = useState(5);
 
-  const indexOfLastItem = itemPerPage * currentPage;
-  const indexOfFirstItem = indexOfLastItem - itemPerPage;
-
-  const arrToPrint = userData.slice(indexOfFirstItem, indexOfLastItem);
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const arr = userData.slice(indexOfFirstItem, indexOfLastItem);
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(userData.length / itemsPerPage); i++) {
+    pages.push(i);
+  }
 
   const next = () => {
     setCurrentPage(currentPage + 1);
+    if (currentPage + 1 > max) {
+      setMax(currentPage + max);
+      setMin(currentPage);
+    }
   };
   const previous = () => {
     setCurrentPage(currentPage - 1);
+    if (currentPage - 1 <= min) {
+      setMax(currentPage - 1);
+      setMin(min - pageLimit);
+    }
+  };
+
+  const jumpPage = (item) => {
+    setCurrentPage(item);
+  };
+
+  const first = () => {
+    setCurrentPage(1);
+    setMax(5);
+    setMin(0);
+  };
+
+  const last = () => {
+    setCurrentPage(Math.ceil(userData.length / itemsPerPage));
+    setMin(Math.ceil(userData.length / itemsPerPage) - max);
+    setMax(Math.ceil(userData.length / itemsPerPage));
   };
 
   return (
-    <div>
-      {arrToPrint.map((item, index) => (
-        <div key={index}>
-          <h3>{item}</h3>
-        </div>
+    <>
+      {arr.map((item, index) => (
+        <h3 key={index}>{item}</h3>
       ))}
-      <div>
-        <button onClick={previous}>Prev</button>
-        <button onClick={next}>Next</button>
-      </div>
-    </div>
+      {currentPage === 1 ? (
+        <>
+          <button disabled={true}>First</button>
+          <button disabled={true}>Prev</button>
+        </>
+      ) : (
+        <>
+          <button onClick={first}>First</button>
+          <button onClick={previous}>Prev</button>
+        </>
+      )}
+
+      {pages.map((item, index) => {
+        if (item > min && item <= max) {
+          return (
+            <button
+              key={index}
+              onClick={() => {
+                jumpPage(item);
+              }}
+            >
+              {item}
+            </button>
+          );
+        }
+      })}
+      {currentPage === Math.ceil(userData.length / itemsPerPage) ? (
+        <>
+          <button disabled={true}>Next</button>
+          <button disabled={true}>Last</button>
+        </>
+      ) : (
+        <>
+          <button onClick={next}>Next</button>
+          <button onClick={last}>Last</button>
+        </>
+      )}
+    </>
   );
 };
 
